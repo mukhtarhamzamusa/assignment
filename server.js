16a13448453552e82e8b55f6776ea87e5c5f8d6c
mukhtarhamzamusa <hamzamusamukhtar@gmail.com>
2025-11-18 14:17:56 -0800

    created gitignore

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import Express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a port number
+const PORT = 3000;
+
+// Endpoint 1: /api/name
+app.get('/api/name', (req, res) => {
+  res.json({
+
+    name: "Mukhtar Hamza Musa",
+    email: "mukhtarhamzamusa67@gmail.com",
+    tasks: [
+      "learn nodejs",
+      "build simple APIs",
+      "practice Express.js"
+    ]
+  });
+});
+
+// Endpoint 2: /api/contact
+app.get('/api/contact', (req, res) => {
+  res.json({
+    email: "mukhtarhamzamusa67@gmail.com",
+    phone: "+2348168564035",
+    address: "Federal University of Technology Babura, Jigawa State"
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
